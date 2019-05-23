0bbe95b7b2a0834ad29f8844dabb2ead022fae87
vadim.gumennyj <vadim.gumennyj@gmail.com>
2019-05-23 17:55:23 +0300

    hide cap when unused

--- src/pages/Home/components/CapComponent.js
+++ src/pages/Home/components/CapComponent.js
@@ -19,11 +19,13 @@ class CapComponent extends React.Component {
       left: props.style.left,
       width: props.style.width,
       height: props.style.height,
+      display: false,
     }
     props.events.addListener(EVENT_MOVE_NEW, this.onMoveStart)
   }
 
   onMoveStart = ({ x0, y0 }) => {
+    this.setState({ display: true })
     this.props.events.addListener(EVENT_MOVE, ({ dx, dy }) => this.onMove({ x0, y0, dx, dy }))
     this.props.events.addListener(EVENT_MOVE_END, () => {
       const { top, left, width, height } = this.state
@@ -41,11 +43,17 @@ class CapComponent extends React.Component {
       left: coordinates.x0,
       width: coordinates.x1 - coordinates.x0,
       height: coordinates.y1 - coordinates.y0,
+      display: true,
     })
   }
 
   render() {
-    const { top, left, width, height } = this.state
+    const { top, left, width, height, display } = this.state
+
+    if (!display) {
+      return null
+    }
+
     return (
       <TouchableWithoutFeedback onPress={this.props.onSelectComponent}>
         <View
